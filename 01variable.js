f9a4d8b1e0c7ff4eec8e4230a661d81900a4e1ac
YASH SHRIVASTAVA <77566023+Yash06-ys@users.noreply.github.com>
2021-02-07 14:24:25 +0530

    Add files via upload

---
+++ 01variable.js
@@ -0,0 +1,34 @@
+var fullname = 'Yash Shrivastava';
+
+console.log('fullname');
+console.log(fullname);
+/*The Above code will display 'fullname' as output because it is within the ''
+The second console will display Yash Shrivastava as the output*/ 
+
+var number = 10; //variable number stores value 10
+console.log(number); //output of number is displayed
+
+var x = 10;
+var y = 20;
+var total = x+y;
+console.log(total);
+/*
+The below code will concatenate the firstname and lastname as the final output of the name
+*/
+var firstname = 'Yash';
+var lastname = 'Shrivastava';
+var name = firstname+" "+lastname;
+console.log(name);
+
+var age;
+age = 21;
+console.log(age);
+/*
+The below code will add the var x and y 
+and will be concatenated with z as z is a string not a number. 
+*/ 
+var x = 2;
+var y = 3;
+var z = '20';
+var final = x+y+z;
+console.log(final);
